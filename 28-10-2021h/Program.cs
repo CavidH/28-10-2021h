0b1b52941ec9edc62b4b624ef1a0c9cb7f9fec40
Cavidh <hcavid386@gmail.com>
2021-10-28 11:05:58 +0400

    Add project files.

---
+++ 28-10-2021h/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _28_10_2021h
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(ReversSentence("salam dunya."));
+        }
+
+        static string ReversSentence(string s)
+        {
+            int Flag = 0;
+            string ResultSentence = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                if (s[i] == '.' || s[i] == ',' || s[i] == ' ' || s[i] == '?' || s[i] == '!' || s[i] == ':')
+                {
+                    ResultSentence += ReverseWord(s, i - 1, Flag);
+                    ResultSentence += s[i];
+                    Flag = i + 1;
+                }
+            }
+            return ResultSentence;
+        }
+        static string ReverseWord(string s, int i, int Flag)
+        {
+            string Result = string.Empty;
+            while (i >= Flag)
+            {
+                Result += s[i];
+                i--;
+            }
+            /**
+             * 
+            for (; i >= Flag; i--)
+            {
+                Result += s[i];
+            }
+            
+         **/
+            return Result;
+        }
+    }
+}
